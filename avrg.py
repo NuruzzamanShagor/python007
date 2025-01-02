20eea459205e1d79e00dea21377a619b119312e2
NuruzzamanShagor <nuruzzaman101325.eee@gmail.com>
2025-01-02 17:12:05 +0600

    first commit

---
+++ avrg.py
@@ -0,0 +1,3 @@
+numbers=[10,20,30,40,50]
+x=sum(numbers)/len(numbers)
+print(f"The average of numbers is {x}")
---
+++ oddnumber.py
@@ -0,0 +1,7 @@
+n=int(input('enter your number')) 
+list=[]
+for i in range(n):
+    if i%2==1:
+        list.append(i)
+
+print(list)
